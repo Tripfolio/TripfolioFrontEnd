f697eb254d51608012ad2a577675a1f82f280469
kiwiisfish <kiwiisfish@gmail.com>
2025-05-20 00:24:12 +0800

    test:測試環境變數引入方式

--- src/views/GoogleMapView.vue
+++ src/views/GoogleMapView.vue
@@ -17,15 +17,15 @@ function loadGoogleMaps() {
       resolve();
       return;
     }
-    const script = document.createElement("script");
-    script.src = `https://maps.googleapis.com/maps/api/js?key=${
-      import.meta.env.VITE_GOOGLE_MAPS_API_KEY
-    }`;
-    script.async = true;
-    script.defer = true;
-    script.onload = resolve;
-    script.onerror = reject;
-    document.head.appendChild(script);
+    // const script = document.createElement("script");
+    // script.src = `https://maps.googleapis.com/maps/api/js?key=${
+    //   import.meta.env.VITE_GOOGLE_MAPS_API_KEY
+    // }`;
+    // script.async = true;
+    // script.defer = true;
+    // script.onload = resolve;
+    // script.onerror = reject;
+    // document.head.appendChild(script);
   });
 }
 onMounted(async () => {
