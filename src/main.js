13ef6aa213a537aa1458a9b05b7048fb03cdce43
Ninahsuuuuu <ninaaahsu@gmail.com>
2025-06-09 19:55:44 +0800

    重複欄位刪除

--- src/main.js
+++ src/main.js
@@ -3,9 +3,6 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faXmark, faPenToSquare } from '@fortawesome/free-solid-svg-icons'
 
 import App from './App.vue'
 import router from './router'
