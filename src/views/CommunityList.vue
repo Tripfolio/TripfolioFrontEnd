56c1f1f82f9f9e61a08ed8f7b0dd6dcf2145f089
Gini <g204208@gmail.com>
2025-06-26 15:04:02 +0800

    style: 新增貼文按鈕樣式

--- src/views/CommunityList.vue
+++ src/views/CommunityList.vue
@@ -28,7 +28,7 @@
 
         <RouterLink
             to="/community/post"
-            class="fixed bottom-6 right-6 w-14 h-14 rounded-full bg-green-500 text-white flex items-center justify-center text-3xl shadow-lg hover:bg-green-600 transition-colors duration-200"
+            class="fixed bottom-6 right-6 w-14 h-14 rounded-full bg-[linear-gradient(45deg,#f7a751,#E1D099,#3CA2D5)] text-white flex items-center justify-center text-3xl shadow-lg hover:bg-green-600 transition-colors duration-200"
         >
             +
         </RouterLink>
