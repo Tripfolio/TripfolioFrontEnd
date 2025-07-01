4cdf589ca3c7409b95b768f51d81bf7b2dc8e6f8
May Su <wannadonna34@gmail.com>
2025-07-02 00:38:27 +0800

    Update LoginPage.vue

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -132,7 +132,7 @@ const login = async () => {
 };
 
 const handleGoogleLogin = () => {
-  window.location.href = `${import.meta.env.VITE_API_URL}/auth/google/callback`;
+  window.location.href = `${import.meta.env.VITE_API_URL}/auth/google`;
 };
 
 const logout = async () => {
